bb2a8964aeec258a0e3909845287e2cd73c65770
Ana Isola <ana.milagros.isola@hotmail.com>
2022-01-28 06:56:25 -0500

    desafio 11 nuevamente

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { NavBar} from './components'
+import { Fragment, useStsate} from 'react'
 
-import HomePage from './Home'
-import CartPage from './Cart'
-import ProductDetailPage from './ProductDetail'
+import "./styles.css";
 
-export default function App() {
-	return(
-		<BrowserRouter>
-		<div className="flex felx-col">
-			<NavBar />
-			<div className='flex-flex-col mt-24'>
+export default function App () {
+	const [loading, setLoading] = useState(true);
 
-   			<Switch>
-				<Route  exact path="/">
-				<HomePage />
-				</Route>
-				<Route path="/cart">
-				 <CartPage />
-				 <Route path="/productId">
-					 <ProductDetailPage />
-				 </Route>
-				</Route>
-			</Switch>
-		</div>
-		</div>
-		</BrowserRouter>
-	)
+	useEffect(() => {
+		setTimeout (() => {
+			setLoading(false);
+		}, 10000);
+	}, []);
+
+	const loaderElement = loading ? <h2>Loading...</h2> : <h3>loaded!</h3>
+
+	return ( 
+	<Fragment>
+		<h1>Example 1</h1>
+		{loading ? <h2>loading...</h2> : <h3>loaded!</h3>}
+		{loaderElement}
+	</Fragment>
+	);
+}
+
+function Example2({ flag }) {
+	if ( flag ) {
+		return {
+			<div> 
+			<h1> El flag es verdadero</h1>
+			</div>
+		};
+	}
+
+	return <h1>El flag es falso</h1>;
+}
+export default function App () {
+	return <Example2 flag/>
+}
+
+function BlueTitle {( children)} {
+	return <h1> style = {{ color: "blue"}} > {chlidren} </h1>
+
+}
+
+function Tecnica5({ colorSchema}) {
+	const TtleComponent = colorSchema === " red" ? RedTitle _ BlueTitle
+	 return <TitleComponent> Soy el titulo</TitleComponent>
+
+}
+
+function ConditionalProps({ flag}) {
+	return <h1 style={{ color: flag ? "red" : " green "}} className={flag ? 'red'}> Soy un titulo </h1>
+}
+
+export default function App {
+	return <ConditionalProps flag />
 }
